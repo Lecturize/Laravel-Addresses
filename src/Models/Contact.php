e8c113cd33be0fc9989eb822abcf9cb16625e725
Alexander Manfred Pöllmann <alex@vendocr.at>
2018-02-21 11:51:10 +0100

    Add contacts model

---
+++ src/Models/Contact.php
@@ -0,0 +1,69 @@
+<?php namespace Lecturize\Addresses\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Lecturize\Addresses\Traits\HasCountry;
+
+/**
+ * Class Contact
+ * @package Lecturize\Addresses\Models
+ */
+class Contact extends Model
+{
+    use HasCountry;
+    use SoftDeletes;
+
+    /**
+     * @inheritdoc
+     */
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'company',
+        'position',
+        'phone',
+        'mobile',
+        'fax',
+        'email',
+        'website',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->table = config('lecturize.contacts.table', 'contacts');
+    }
+
+    /**
+     * Get the related model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function contactable()
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Get the validation rules.
+     *
+     * @return array
+     */
+    public static function getValidationRules()
+    {
+        $rules = [];
+
+        return $rules;
+    }
+}
