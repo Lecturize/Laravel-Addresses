a831b02add972e66d6a3a62796b981bf29c197e0
AlexanderPoellmann <alex@lecturize.com>
2020-11-14 15:25:14 +0100

    Add testing boilerplate

---
+++ tests/DemoTest.php
@@ -0,0 +1,9 @@
+<?php namespace Lecturize\Addresses\Tests;
+
+class DemoTest extends TestCase
+{
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php namespace Lecturize\Addresses\Tests;
+
+use Lecturize\Addresses\AddressesServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        // additional setup
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            AddressesServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
+}
