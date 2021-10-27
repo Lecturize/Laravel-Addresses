ebc8ad11544143d7cf1750653799b7730918f1d2
Oleg Pashkovsky <oleg@pashkovsky.me>
2021-10-27 13:16:45 +0300

    cleanup

--- config/config.php
+++ config/config.php
@@ -2,39 +2,6 @@
 
 return [
     /*
-     * Addresses
-     */
-    'addresses' => [
-        /*
-         * Main table.
-         */
-        'table' => 'addresses',
-
-        /*
-         * Flag columns to be added to table.
-         */
-        'flags' => ['public', 'primary', 'billing', 'shipping'],
-
-        /*
-         * The validation rules for an address.
-         */
-        'rules' => [
-            'street'       => 'required|string|min:3|max:60',
-            'street_extra' => 'nullable|string|min:3|max:60',
-            'city'         => 'required|string|min:3|max:60',
-            'state'        => 'nullable|string|min:3|max:60',
-            'post_code'    => 'required|min:4|max:10|AlphaDash',
-            'country_id'   => 'required|integer',
-        ],
-
-        /*
-         * Enable geocoding to add coordinates (lon/lat) to addresses.
-         * Default: false
-         */
-        'geocode' => false,
-    ],
-
-    /*
      * Contacts
      */
     'contacts' => [
