cd0ada7fbb5119db258b44b6a24db1722a92e682
Oleg Pashkovsky <oleg@pashkovsky.me>
2021-10-27 14:19:30 +0300

    Fix migrations

--- src/ContactsServiceProvider.php
+++ src/ContactsServiceProvider.php
@@ -13,6 +13,7 @@ class ContactsServiceProvider extends ServiceProvider
     /** @var string[]|array */
     protected $migrations = [
         'CreateContactsTable'  => 'create_contacts_table',
+        'CreateContactValuesTable' => 'create_contact_values_table',
     ];
 
     public function boot()
